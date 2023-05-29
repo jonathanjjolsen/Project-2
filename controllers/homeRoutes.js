69d3df0379f96433617dcd6c42b492338b3264ab
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-05-28 23:41:31 -0700

    Adjusted routes, seeds, and handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Item, User } = require('../models');
+const { Item, User, Category } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -11,17 +11,18 @@ router.get('/', async (req, res) => {
           model: User,
           attributes: ['name'],
         },
+        {
+          model: Category,
+          attributes: ['name'],
+        }
       ],
     });
 
     // Serialize data so the template can read it
     const items = itemData.map((item) => item.get({ plain: true }));
-    console.log(items);
-
     // Pass serialized data and session flag into template
     res.render('homepage', { 
       items, 
-      logged_in: req.session.logged_in 
     });
   } catch (err) {
     res.status(500).json(err);
